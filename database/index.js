7e9c99e4148fbdcc4c7bf000e3fcb226be85348f
Joey Nguyen <joeylnguyen95@gmail.com>
2020-04-24 13:49:04 -0700

    Create repoSchema

--- database/index.js
+++ database/index.js
@@ -3,6 +3,12 @@ mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
   // TODO: your schema here!
+  repo_name: String, // name
+  repo_owner: String, // owner.login
+  repo_owner_url: String, // ownerurl
+  repo_url: String, // html_url
+  repo_id: Number, // id
+  fork_count: Number // forks
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
